a6cb58acd2842b56ba38d405f6cf98def11d36e4
spacex81 <spacex81@gmail.com>
2024-03-12 16:22:22 +0900

    d

--- server.js
+++ server.js
@@ -1,56 +1,44 @@
-let port = process.env.PORT || 8080;
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-let IO = require("socket.io")(port, {
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 8080;
+const io = socketIo(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-IO.use((socket, next) => {
-  if (socket.handshake.query) {
-    let callerId = socket.handshake.query.callerId;
-    socket.user = callerId;
-    next();
+    methods: ["GET", "POST"]
   }
 });
 
-IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
+io.use((socket, next) => {
+  if (socket.handshake.query && socket.handshake.query.callerId) {
+    let callerId = socket.handshake.query.callerId;
+    socket.user = callerId;
+    next();
+  } else {
+    next(new Error('Authentication error'));
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`${socket.user} Connected`);
   socket.join(socket.user);
 
-  socket.on("makeCall", (data) => {
-    console.log("makeCall")
+  socket.on('makeCall', (data) => {
     let calleeId = data.calleeId;
     let sdpOffer = data.sdpOffer;
 
-    socket.to(calleeId).emit("newCall", {
+    socket.to(calleeId).emit('newCall', {
       callerId: socket.user,
       sdpOffer: sdpOffer,
     });
   });
 
-  socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
-
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
-    });
-  });
-
-  socket.on("IceCandidate", (data) => {
-    let calleeId = data.calleeId;
-    let iceCandidate = data.iceCandidate;
-
-    socket.to(calleeId).emit("IceCandidate", {
-      sender: socket.user,
-      iceCandidate: iceCandidate,
-    });
-  });
+  // Add other event listeners here
 });
 
-IO.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
